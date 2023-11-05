746999728cc4d8bebb6b3ac6ad8d805b8d64518d
Galina A. Basargina <gala.basargina@yandex.ru>
2023-11-05 20:53:22 +0300

    работа с несколькими фрагментами на одной карте, выбор наиболее правдоподобных

--- README.md
+++ README.md
@@ -419,24 +419,21 @@ for angle in range(0, 4):
     # вычисляем размеры повёрнутой картинки (у квадратов они одинаковые)
     point1_w, point1_h = (point1.shape[1], point1.shape[0])
     assert point1_original_w == point1_w
-    print(angle*90, 'wh', point1_w, point1_h, 'dsize', dsize, 'shape', point1.shape, 'center', center1_x, center1_y)
+    # print(angle*90, 'wh', point1_w, point1_h, 'dsize', dsize, 'shape', point1.shape, 'center', center1_x, center1_y)
     # добавление повернутых изображений в список
     point1_rotated.append((angle*90, point1.copy(), point1_w, point1_h, 1.0))
     # теперь ресайзим изображения (уменьшаем)
     dsize = (int((1.0-RESIZE_PERCENT)*point1_original_w), int((1.0-RESIZE_PERCENT)*point1_original_h))
-    print(dsize)
     point1_reduced = cv2.resize(point1, dsize)
     point1_rotated.append((angle*90, point1_reduced.copy(), dsize[0], dsize[1], 1.0-RESIZE_PERCENT))
     # увеличиваем
     dsize = (int((1.1 + RESIZE_PERCENT) * point1_original_w), int((1.1 + RESIZE_PERCENT) * point1_original_h))
-    print(dsize)
     point1_enlarged = cv2.resize(point1, dsize)
     point1_rotated.append((angle * 90, point1_enlarged.copy(), dsize[0], dsize[1], 1.0+RESIZE_PERCENT))
     # удаляем скопированные объекты
     del point1_reduced
     del point1
     del M
-
 
 frame = map
 # составляем все версии всех найденных правдопободных сравнений
@@ -476,3 +473,142 @@ cv2.destroyAllWindows()
 Теперь выбран наиболее подходящий вариант:
 
 <img src="https://raw.githubusercontent.com/Galina-Basargina/groscan_hacaton/main/imgs/bear_05112023-20.png" width="100%">
+
+### Поиск нескольких фрагментов одновременно и выбор наиболее правдоподобных
+
+Добавляем новый фрагмент, теперь при поиске их используется два:
+
+<img src="https://raw.githubusercontent.com/Galina-Basargina/groscan_hacaton/main/imgs/bear-point1.png">
+<img src="https://raw.githubusercontent.com/Galina-Basargina/groscan_hacaton/main/imgs/bear-point2.png">
+
+```python
+import cv2
+import numpy as np
+
+THRESHOLD = 0.80  # степень правдоподобности 80% и выше
+RESIZE_PERCENT = 0.13
+
+cv2.namedWindow("Pioneer View", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Point1", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Point2", cv2.WINDOW_NORMAL)
+
+map = cv2.imread("imgs/bear-enlarged.png")
+# настройки поиска элементов на карте
+point1_original = cv2.imread("imgs/bear-point1.png")
+point2_original = cv2.imread("imgs/bear-point2.png")
+points_original = [
+    (1, point1_original),  # первая точка и ей соответствующий маркер
+    (2, point2_original)   # вторая точка и ей соответствующий маркер
+]
+
+#  список всех модифицированных фрагментов
+point_rotated = []
+# перебор всех элементов и подготовка фрагментов к поиску на карте
+for (point_number, point_original) in points_original:
+    # проверяем, что размеры картинок совпадают, и они являются квадратами, иначе методы поворота не работают
+    # (поворот выполняется за пределы картинки, если центр вращения - центр прямоугольника)
+    assert point_original.shape[1] == point_original.shape[0]
+    # в shape при загрузке картинки идёт сначала ширина, а потом высота
+    point_original_w, point_original_h = (point_original.shape[1], point_original.shape[0])
+    center_x, center_y = (point_original_w // 2, point_original_h // 2)
+    # поворот искомой картинки (4 раза по 90 градусов)
+    for angle in range(0, 4):
+        # создаём матрицу поворота
+        M = cv2.getRotationMatrix2D((center_x, center_y), angle*90, 1.0)
+        # вычисляем размеры повёрнутой картинки (так и не удалось повернуть прямоугольник)
+        dsize = (point_original_w, point_original_h)
+        # поворачиваем картинку и маску
+        point = cv2.warpAffine(point_original, M, dsize)
+        # вычисляем размеры повёрнутой картинки (у квадратов они одинаковые)
+        point_w, point_h = (point.shape[1], point.shape[0])
+        assert point_original_w == point_w
+        # print(angle*90, 'wh', point_w, point_h, 'dsize', dsize, 'shape', point.shape, 'center', center_x, center_y)
+        # добавление повернутых изображений в список
+        point_rotated.append((
+            point_number,
+            angle*90,
+            point.copy(), point_w, point_h,
+            1.0))
+        # теперь ресайзим изображения (уменьшаем)
+        dsize = (int((1.0-RESIZE_PERCENT)*point_original_w), int((1.0-RESIZE_PERCENT)*point_original_h))
+        point_reduced = cv2.resize(point, dsize)
+        point_rotated.append((
+            point_number,
+            angle*90,
+            point_reduced.copy(), dsize[0], dsize[1],
+            1.0-RESIZE_PERCENT))
+        # увеличиваем
+        dsize = (int((1.1 + RESIZE_PERCENT) * point_original_w), int((1.1 + RESIZE_PERCENT) * point_original_h))
+        point_enlarged = cv2.resize(point, dsize)
+        point_rotated.append((
+            point_number,
+            angle * 90,
+            point_enlarged.copy(), dsize[0], dsize[1],
+            1.0+RESIZE_PERCENT))
+        # удаляем скопированные объекты
+        del point_enlarged
+        del point_reduced
+        del point
+        del M
+
+frame = map
+# составляем все версии всех найденных правдопободных сравнений
+versions = []
+for (point_number, angle, point, point_w, point_h, resize) in point_rotated:
+    result = cv2.matchTemplate(frame, point, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    # print(point_number, angle, min_val, max_val, min_loc, max_loc, resize)
+    if max_val >= THRESHOLD:
+        if max_loc:
+            versions.append((
+                point_number,
+                result,
+                min_val, max_val, min_loc, max_loc,
+                angle, point_w, point_h, resize))
+# если объекты найдены
+if versions:
+    for (point_number, _, min_val, max_val, min_loc, max_loc, angle, _, _, resize) in versions:
+        print('version:', point_number, angle, min_val, max_val, min_loc, max_loc, resize)
+    # составляем список правдоподобных точек, в которых мы находимся
+    point_numbers = set([v[0] for v in versions])
+    # сортируем список в порядке уменьшения правдоподобности
+    print('points:', point_numbers, [(x[0],x[3]) for x in versions])
+    versions.sort(key=lambda x: x[3], reverse=True)
+    print('points:', point_numbers, [(x[0],x[3]) for x in versions], 'best:', versions[0][0])
+    # перебираем все правдоподобные точки
+    for point_number in point_numbers:
+        # берём первый наиболее правдоподобный элемент в отсортированном списке
+        # (хотя можно перебрать их все)
+        # Внимание! в этом списке м.б. несколько версий одной и той же точки (напр., после ресайза)
+        (_,
+         result,
+         min_val, max_val, min_loc, max_loc,
+         angle, point_w, point_h,
+         resize) = next((v for v in versions if v[0] == point_number), None)
+        # выводим результат
+        (x, y) = max_loc
+        cv2.rectangle(frame, (x, y), (x + point_w-1, y + point_h-1), (0, 0, 255), 5)
+        cv2.rectangle(result, (x, y), (x + point_w-1, y + point_h-1), (255, 255, 255), 1)
+        cv2.imshow("Point{}".format(point_number), result)
+del versions
+
+cv2.imshow("Pioneer View", frame)
+cv2.resizeWindow("Pioneer View", 800, 600)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
+```
+
+На экране появятся измерения правдоподобности:
+
+```text
+version: 1 0 -0.40340617299079895 0.8048402070999146 (423, 323) (42, 74) 1.0
+version: 1 0 -0.3985312581062317 0.9430774450302124 (270, 263) (23, 29) 1.13
+version: 2 0 -0.4041024446487427 0.907184898853302 (0, 82) (665, 294) 1.13
+points: {1, 2} [(1, 0.8048402070999146), (1, 0.9430774450302124), (2, 0.907184898853302)]
+points: {1, 2} [(1, 0.9430774450302124), (2, 0.907184898853302), (1, 0.8048402070999146)] best: 1
+```
+
+Результат:
+
+<img src="https://raw.githubusercontent.com/Galina-Basargina/groscan_hacaton/main/imgs/bear_05112023-21.png" width="100%">
--- bear.py
+++ bear.py
@@ -6,73 +6,106 @@ RESIZE_PERCENT = 0.13
 
 cv2.namedWindow("Pioneer View", cv2.WINDOW_NORMAL)
 cv2.namedWindow("Point1", cv2.WINDOW_NORMAL)
+cv2.namedWindow("Point2", cv2.WINDOW_NORMAL)
 
 map = cv2.imread("imgs/bear-enlarged.png")
-point1_original = cv2.imread("imgs/bear-small.png")
-# проверяем, что размеры картинок совпадают, и они являются квадратами, иначе методы поворота не работают
-# (поворот выполняется за пределы картинки, если центр вращения - центр прямоугольника)
-assert point1_original.shape[1] == point1_original.shape[0]
-# в shape при загрузке картинки идёт сначала ширина, а потом высота
-point1_original_w, point1_original_h = (point1_original.shape[1], point1_original.shape[0])
-center1_x, center1_y = (point1_original_w // 2, point1_original_h // 2)
+# настройки поиска элементов на карте
+point1_original = cv2.imread("imgs/bear-point1.png")
+point2_original = cv2.imread("imgs/bear-point2.png")
+points_original = [
+    (1, point1_original),  # первая точка и ей соответствующий маркер
+    (2, point2_original)   # вторая точка и ей соответствующий маркер
+]
 
-# поворот искомой картинки (4 раза по 90 градусов)
-point1_rotated = []
-for angle in range(0, 4):
-    # создаём матрицу поворота
-    M = cv2.getRotationMatrix2D((center1_x, center1_y), angle*90, 1.0)
-    # вычисляем размеры повёрнутой картинки (так и не удалось повернуть прямоугольник)
-    dsize = (point1_original_w, point1_original_h)
-    # поворачиваем картинку и маску
-    point1 = cv2.warpAffine(point1_original, M, dsize)
-    # вычисляем размеры повёрнутой картинки (у квадратов они одинаковые)
-    point1_w, point1_h = (point1.shape[1], point1.shape[0])
-    assert point1_original_w == point1_w
-    print(angle*90, 'wh', point1_w, point1_h, 'dsize', dsize, 'shape', point1.shape, 'center', center1_x, center1_y)
-    # добавление повернутых изображений в список
-    point1_rotated.append((angle*90, point1.copy(), point1_w, point1_h, 1.0))
-    # теперь ресайзим изображения (уменьшаем)
-    dsize = (int((1.0-RESIZE_PERCENT)*point1_original_w), int((1.0-RESIZE_PERCENT)*point1_original_h))
-    print(dsize)
-    point1_reduced = cv2.resize(point1, dsize)
-    point1_rotated.append((angle*90, point1_reduced.copy(), dsize[0], dsize[1], 1.0-RESIZE_PERCENT))
-    # увеличиваем
-    dsize = (int((1.1 + RESIZE_PERCENT) * point1_original_w), int((1.1 + RESIZE_PERCENT) * point1_original_h))
-    print(dsize)
-    point1_enlarged = cv2.resize(point1, dsize)
-    point1_rotated.append((angle * 90, point1_enlarged.copy(), dsize[0], dsize[1], 1.0+RESIZE_PERCENT))
-    # удаляем скопированные объекты
-    del point1_reduced
-    del point1
-    del M
-
+#  список всех модифицированных фрагментов
+point_rotated = []
+# перебор всех элементов и подготовка фрагментов к поиску на карте
+for (point_number, point_original) in points_original:
+    # проверяем, что размеры картинок совпадают, и они являются квадратами, иначе методы поворота не работают
+    # (поворот выполняется за пределы картинки, если центр вращения - центр прямоугольника)
+    assert point_original.shape[1] == point_original.shape[0]
+    # в shape при загрузке картинки идёт сначала ширина, а потом высота
+    point_original_w, point_original_h = (point_original.shape[1], point_original.shape[0])
+    center_x, center_y = (point_original_w // 2, point_original_h // 2)
+    # поворот искомой картинки (4 раза по 90 градусов)
+    for angle in range(0, 4):
+        # создаём матрицу поворота
+        M = cv2.getRotationMatrix2D((center_x, center_y), angle*90, 1.0)
+        # вычисляем размеры повёрнутой картинки (так и не удалось повернуть прямоугольник)
+        dsize = (point_original_w, point_original_h)
+        # поворачиваем картинку и маску
+        point = cv2.warpAffine(point_original, M, dsize)
+        # вычисляем размеры повёрнутой картинки (у квадратов они одинаковые)
+        point_w, point_h = (point.shape[1], point.shape[0])
+        assert point_original_w == point_w
+        # print(angle*90, 'wh', point_w, point_h, 'dsize', dsize, 'shape', point.shape, 'center', center_x, center_y)
+        # добавление повернутых изображений в список
+        point_rotated.append((
+            point_number,
+            angle*90,
+            point.copy(), point_w, point_h,
+            1.0))
+        # теперь ресайзим изображения (уменьшаем)
+        dsize = (int((1.0-RESIZE_PERCENT)*point_original_w), int((1.0-RESIZE_PERCENT)*point_original_h))
+        point_reduced = cv2.resize(point, dsize)
+        point_rotated.append((
+            point_number,
+            angle*90,
+            point_reduced.copy(), dsize[0], dsize[1],
+            1.0-RESIZE_PERCENT))
+        # увеличиваем
+        dsize = (int((1.1 + RESIZE_PERCENT) * point_original_w), int((1.1 + RESIZE_PERCENT) * point_original_h))
+        point_enlarged = cv2.resize(point, dsize)
+        point_rotated.append((
+            point_number,
+            angle * 90,
+            point_enlarged.copy(), dsize[0], dsize[1],
+            1.0+RESIZE_PERCENT))
+        # удаляем скопированные объекты
+        del point_enlarged
+        del point_reduced
+        del point
+        del M
 
 frame = map
 # составляем все версии всех найденных правдопободных сравнений
 versions = []
-for (angle, point1, point1_w, point1_h, resize) in point1_rotated:
-    result = cv2.matchTemplate(frame, point1, cv2.TM_CCOEFF_NORMED)
+for (point_number, angle, point, point_w, point_h, resize) in point_rotated:
+    result = cv2.matchTemplate(frame, point, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    # print(angle, min_val, max_val, min_loc, max_loc, resize)
+    # print(point_number, angle, min_val, max_val, min_loc, max_loc, resize)
     if max_val >= THRESHOLD:
         if max_loc:
             versions.append((
+                point_number,
                 result,
                 min_val, max_val, min_loc, max_loc,
-                angle, point1_w, point1_h, resize))
+                angle, point_w, point_h, resize))
 # если объекты найдены
 if versions:
+    for (point_number, _, min_val, max_val, min_loc, max_loc, angle, _, _, resize) in versions:
+        print('version:', point_number, angle, min_val, max_val, min_loc, max_loc, resize)
+    # составляем список правдоподобных точек, в которых мы находимся
+    point_numbers = set([v[0] for v in versions])
     # сортируем список в порядке уменьшения правдоподобности
-    versions.sort(key=lambda x: x[2], reverse=True)
-    # берём первый наиболее правдоподобный элемент (хотя можно перебрать их все)
-    (result,
-     min_val, max_val, min_loc, max_loc,
-     angle, point1_w, point1_h, resize) = versions[0]
-    # выводим результат
-    (x, y) = max_loc
-    cv2.rectangle(frame, (x, y), (x + point1_w-1, y + point1_h-1), (0, 0, 255), 5)
-    cv2.rectangle(result, (x, y), (x + point1_w-1, y + point1_h-1), (255, 255, 255), 1)
-    cv2.imshow("Point1", result)
+    print('points:', point_numbers, [(x[0],x[3]) for x in versions])
+    versions.sort(key=lambda x: x[3], reverse=True)
+    print('points:', point_numbers, [(x[0],x[3]) for x in versions], 'best:', versions[0][0])
+    # перебираем все правдоподобные точки
+    for point_number in point_numbers:
+        # берём первый наиболее правдоподобный элемент в отсортированном списке
+        # (хотя можно перебрать их все)
+        # Внимание! в этом списке м.б. несколько версий одной и той же точки (напр., после ресайза)
+        (_,
+         result,
+         min_val, max_val, min_loc, max_loc,
+         angle, point_w, point_h,
+         resize) = next((v for v in versions if v[0] == point_number), None)
+        # выводим результат
+        (x, y) = max_loc
+        cv2.rectangle(frame, (x, y), (x + point_w-1, y + point_h-1), (0, 0, 255), 5)
+        cv2.rectangle(result, (x, y), (x + point_w-1, y + point_h-1), (255, 255, 255), 1)
+        cv2.imshow("Point{}".format(point_number), result)
 del versions
 
 cv2.imshow("Pioneer View", frame)
